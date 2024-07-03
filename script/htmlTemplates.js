8ce0d7ebeaa513f198ec3e8325539019b3440cbe
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-07-03 13:35:39 +0200

    Squashed commit of the following:
    commit cd12c69f94d7058ceffdb640c4ebec78c3b66fc4
    Author: BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
    Date:   Wed Jul 3 13:34:13 2024 +0200
    
        finish filter
    
    commit 670b2238b35e5f29200e200271a26bac3bce28e9
    Author: BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
    Date:   Wed Jul 3 04:15:28 2024 +0200
    
        add filter function
    
    commit 2e3fc86393926cd9e264bcc887895f5de3b8baca
    Author: BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
    Date:   Wed Jul 3 01:50:46 2024 +0200
    
        revise JS files add d-pad function, favicon

---
+++ script/htmlTemplates.js
@@ -0,0 +1,277 @@
+function htmlRenderEvoPoke(pokeIndex = emptyPoke, step, index = 0) {
+  return /*html*/`
+    <div id="${step + index}" class="evoStepPokemon">
+      <img src="${pokeIndex.imgBig}" alt="imageOf${pokeIndex.origin}">
+      <p>${pokeIndex.name}</p>
+      <p>${String(pokeIndex.id).padStart(4, '0')}</p>
+      ${(step != 'firstEvolution') && (step != 'secondEvolution') ?
+      '' :
+      (evolutionChain[evolutionChain.findIndex(ele => pokeIndex.evolution == ele.id)][step][index].trigger == 'level-up') && (evolutionChain[evolutionChain.findIndex(ele => pokeIndex.evolution == ele.id)][step][index].level != null) ?
+        /*html*/`<p>ab Level ${evolutionChain[evolutionChain.findIndex(ele => pokeIndex.evolution == ele.id)][step][index].level}</p>` :
+        ''}
+    </div>
+  `;
+}
+
+function htmlRenderEvo(step) {
+  return /*html*/`
+    <div id="${step}" class="evoStepContainer"></div>
+  `;
+}
+
+function htmlDamageList(counterPoke) {
+  return /*html*/`
+        <li><img src="${counterPoke.img}" alt="${counterPoke.origin}TypeImage"></li>
+      `;
+}
+
+function htmlDamageEmpty(x) {
+  return /*html*/`
+      ${x == 'start' ? '<ul>' : '</ul>'}
+    `;
+}
+
+function htmlrenderMisc(pokemonId) {
+  return /*html*/`
+    <img src="${pokemon[pokemonId].imgBig}" alt="image of ${pokemon[pokemonId].origin}">
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>${pokemon[pokemonId].name}</td>
+          </tr>
+          <tr>
+            <td>Größe</td>
+            <td>${Number(pokemon[pokemonId].height / 10).toFixed(1)} Meter</td>
+          </tr>
+          <tr>
+            <td>Gewicht</td>
+            <td>${Number(pokemon[pokemonId].height / 10).toFixed(1)} Kilogramm</td>
+          </tr>
+          <tr>
+            <td>Art</td>
+            <td>${pokemon[pokemonId].genus}</td>
+          </tr>
+          <tr>
+            <td>Info</td>
+            <td>${pokemon[pokemonId].flavorText}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `;
+}
+
+function htmlrenderStats(pokemonId) {
+  return /*html*/`
+    <div class="statOverview">
+      <div class="statsCont">
+        <div class="statContainer">
+          <div class="statBar statHp" style="width: ${Number(pokemon[pokemonId].stats.hp / 255 * 100).toFixed()}%;"></div>
+        </div>
+        <div>
+          <p>HP</p>
+          <p>${pokemon[pokemonId].stats.hp}</p>
+        </div>
+      </div>
+      <div class="statsCont">
+        <div class="statContainer">
+          <div class="statBar statAttack" style="width: ${Number(pokemon[pokemonId].stats.attack / 255 * 100).toFixed()}%;"></div>
+        </div>
+        <div>
+	        <p>Angriff</p>
+          <p>${pokemon[pokemonId].stats.attack}</p>
+        </div>
+      </div>
+      <div class="statsCont">
+        <div class="statContainer">
+          <div class="statBar statDefense" style="width: ${Number(pokemon[pokemonId].stats.defense / 255 * 100).toFixed()}%;"></div>
+        </div>
+        <div>
+	        <p>Verteidigung</p>
+          <p>${pokemon[pokemonId].stats.defense}</p>
+        </div>
+      </div>
+      <div class="statsCont">
+        <div class="statContainer">
+          <div class="statBar statSpAttack" style="width: ${Number(pokemon[pokemonId].stats.spAttack / 255 * 100).toFixed()}%;"></div>
+        </div>        
+        <div>
+	        <p>Spezial Angriff</p>
+          <p>${pokemon[pokemonId].stats.spAttack}</p>
+        </div>
+      </div>
+      <div class="statsCont">
+        <div class="statContainer">
+          <div class="statBar statSpDefense" style="width: ${Number(pokemon[pokemonId].stats.spDefense / 255 * 100).toFixed()}%;"></div>
+        </div>
+        <div>
+	        <p>Spezial Verteidigung</p>
+          <p>${pokemon[pokemonId].stats.spDefense}</p>
+        </div>
+      </div>
+      <div class="statsCont">
+        <div class="statContainer">
+          <div class="statBar statSpeed" style="width: ${Number(pokemon[pokemonId].stats.speed / 255 * 100).toFixed()}%;"></div>
+        </div>
+        <div>
+	        <p>Geschwindigeit</p>
+          <p>${pokemon[pokemonId].stats.speed}</p>
+        </div>
+      </div>
+    </div>
+    <div class="damageOverview">
+      <div class="damageTypes">
+        <img src="${types[types.findIndex((element) => element.origin === pokemon[pokemonId].type1)].img}" alt="${pokemon[pokemonId].type1}TypeImage">
+        <div class="damageMulti">
+          <ul>
+            <li>Doppelter Schaden gegen</li>
+            ${renderDamage(pokemonId, 'double_damage_to', 'type1')}
+            <li>Halber Schaden von</li>
+            ${renderDamage(pokemonId, 'half_damage_from', 'type1')}
+            <li>Keinen Schaden von</li>
+            ${renderDamage(pokemonId, 'no_damage_from', 'type1')}
+          </ul>
+        </div>
+        <div class="damageMulti">
+          <ul>
+            <li>Doppelter Schaden von</li>
+            ${renderDamage(pokemonId, 'double_damage_from', 'type1')}
+            <li>Halber Schaden gegen</li>
+            ${renderDamage(pokemonId, 'half_damage_to', 'type1')}
+            <li>Keinen Schaden gegen</li>
+            ${renderDamage(pokemonId, 'no_damage_to', 'type1')}
+          </ul>
+        </div>
+      </div>
+      ${pokemon[pokemonId].type2 === '' ? '' : /*html*/`
+      <div class="damageTypes">
+        <img src="${types[types.findIndex((element) => element.origin === pokemon[pokemonId].type2)].img}" alt="${pokemon[pokemonId].type2}TypeImage">
+        <div class="damageMulti">
+          <ul>
+            <li>Doppelter Schaden gegen</li>
+            ${renderDamage(pokemonId, 'double_damage_to', 'type2')}
+            <li>Halber Schaden von</li>
+            ${renderDamage(pokemonId, 'half_damage_from', 'type2')}
+            <li>Keinen Schaden von</li>
+            ${renderDamage(pokemonId, 'no_damage_from', 'type2')}
+          </ul>
+        </div>
+        <div class="damageMulti">
+          <ul>
+            <li>Doppelter Schaden von</li>
+            ${renderDamage(pokemonId, 'double_damage_from', 'type2')}
+            <li>Halber Schaden gegen</li>
+            ${renderDamage(pokemonId, 'half_damage_to', 'type2')}
+            <li>Keinen Schaden gegen</li>
+            ${renderDamage(pokemonId, 'no_damage_to', 'type2')}
+          </ul>
+        </div>
+      </div>
+      ` }
+    </div>
+  `;
+}
+
+function htmlRenderBars(pokeIndex = pokemon[currentId]) {
+  if (pokeIndex == 'hidden') {
+    return /*html*/`
+      <div class="pokeBar noPointer"></div>
+    `;
+  } else {
+    return /*html*/`
+    <div class="pokeBar" onclick="changePoke(${pokeIndex.id})" data-type1=${pokeIndex.type1} ${pokeIndex.type2 !== '' ? `data-type2=${pokeIndex.type2}` : ''}>
+      <div class="barStatus">
+        <p class="PokeId">${String(pokeIndex.id).padStart(4, '0')}</p>
+        <img src="${pokeIndex.imgSmall}" alt="image of ${pokeIndex.origin}">
+        <p class="pokeName">${pokeIndex.name}</p>
+      </div>
+      <div class="barType">
+        <img src="${types[types.findIndex((element) => element.origin === pokeIndex.type1)].img}" alt="${pokeIndex.type1}TypeImage">
+        ${pokeIndex.type2 !== '' ? `<img src="${types[types.findIndex((element) => element.origin === pokeIndex.type2)].img}" alt="${pokeIndex.type1}TypeImage">` : ''}
+      </div>
+    </div>
+    `;
+  }
+}
+
+function htmlRenderCards(pokeArray = pokemon, pokeId = 0) {
+  return /*html*/`
+    <div id="pokemon${pokeId}" class="pokeCard" onclick="changePoke(${pokeArray[pokeId].id})" data-type1=${pokeArray[pokeId].type1} ${pokeArray[pokeId].type2 !== '' ? `data-type2=${pokeArray[pokeId].type2}` : ''}>
+      <div class="cardStatus">
+        <p class="PokeId">${String(pokeArray[pokeId].id).padStart(4, '0')}</p>
+        <img src="${pokeArray[pokeId].imgSmall}" alt="image of ${pokeArray[pokeId].origin}">
+        <p class="pokeName">${pokeArray[pokeId].name}</p>
+      </div>
+      <div class="cardType">
+      <img loading="lazy"
+        src="${types[types.findIndex((element) => element.origin === pokeArray[pokeId].type1)].img}" alt="${pokeArray[pokeId].type1}TypeImage">
+        ${pokeArray[pokeId].type2 !== '' ? `<img loading="lazy" src="${types[types.findIndex((element) => element.origin === pokeArray[pokeId].type2)].img}" alt="${pokeArray[pokeId].type1}TypeImage">` : ''}
+  </div>
+</div>
+`;
+}
+
+function htmlCreateCardPage(page) {
+  document.getElementById('pagination').innerHTML += /*html*/`
+    <a href="#cardView${page}" onclick="openCard(${page}, this)" ${page == 1 ? `id="defaultCard"` : ''}>${page}</a>
+  `;
+  document.getElementById('cardView').innerHTML += /*html*/`
+    <div id="cardView${page}" class="cardView ${page == 1 ? '' : "d-none"}"></div>
+  `;
+}
+
+function htmlSaveQuestion() {
+  return /*html*/`
+    <p>Möchtest du dein Datenset speichern um nächstes mal schneller loszulegen?</p>
+    <div>
+      <button onclick="userResponse(true)">JA</button>
+      <button onclick="userResponse(false)">NEIN</button>
+    </div>
+  `;
+};
+
+function htmlSaveFinish() {
+  return /*html*/`
+    <p>Daten wurden gespeichert viel Spaß auf deiner Reise.</p>
+    <button onclick="document.getElementById('topScreenInfo').classList.toggle('d-none')">Schließen</button>
+  `;
+}
+
+function htmlSaveAbort() {
+  return /*html*/`
+  <p>Daten wurden nicht gespeichert viel Spaß auf deiner Reise.</p>
+  <button onclick="document.getElementById('topScreenInfo').classList.toggle('d-none')">Schließen</button>
+`;
+}
+
+function htmlStart() {
+  return /*html*/`
+    <p>Bisher sind noch keine Einträge in deinem Pokedex vorhanden.</p>
+    <p>Möchtest du Professor Eich um Hilfe Fragen und dir das Aktuelle Datenset herunterladen?</p>
+    <div>
+      <button onclick="userResponse(true)">JA</button>
+      <button onclick="userResponse(false)">NEIN</button>
+    </div>
+  `;
+}
+
+function htmlContinue() {
+  return /*html*/`
+    <p>Daten werder Geladen. Es beginnt sobald genügend Daten vorhanden sind</p>
+    <div id="loaderContainer" class="loaderContainer"></div>
+  `;
+}
+
+function htmlLoaderAdd() {
+  return /*html*/`
+    <div class="loader"></div>
+  `;
+}
+
+function htmlAbort() {
+  return /*html*/`
+    <p>Schade, viel Spaß auf deiner Reise. Solltest du es dir anders Überlegen Starte das Gerät neu</p>
+  `;
+}
---
+++ script/objectTemplates.js
@@ -0,0 +1,78 @@
+function pushEvolution(evo, evoChainIndex) {
+  let firstEvolution = [];
+  let secondEvolution = [];
+  evolutionChain.push({
+    'id': evo.id,
+    'basePoke': evo.chain.species.name,
+    'firstEvolution': [],
+    'secondEvolution': [],
+  });
+  firstEvo(evo, evoChainIndex);
+}
+
+function firstEvo(evo, evoChainIndex) {
+  let firstEvolution = [];
+  let secondEvolution;
+  if (evo.chain.evolves_to.length > 0) {
+    for (let i = 0; i < evo.chain.evolves_to.length; i++) {
+      firstEvolution.push({
+        'name': evo.chain.evolves_to[i].species.name,
+        'trigger': evo.chain.evolves_to[i].evolution_details.length > 0 ? evo.chain.evolves_to[i].evolution_details[0].trigger.name : null,
+        'level': evo.chain.evolves_to[i].evolution_details.length > 0 ? evo.chain.evolves_to[i].evolution_details[0].trigger.name !== 'level-up' ? null : evo.chain.evolves_to[i].evolution_details[0].min_level : null,
+      });
+      second = secondEvo(evo, i);
+      secondEvolution = second;
+    }
+  }
+  evolutionChain[evoChainIndex].firstEvolution = firstEvolution;
+  evolutionChain[evoChainIndex].secondEvolution = secondEvolution;
+}
+
+function secondEvo(evo, firstIndex) {
+  let secondEvolution = [];
+  if (evo.chain.evolves_to[firstIndex].evolves_to.length > 0) {
+    for (let j = 0; j < evo.chain.evolves_to[firstIndex].evolves_to.length; j++) {
+      secondEvolution.push({
+        'name': evo.chain.evolves_to[firstIndex].evolves_to[j].species.name,
+        'trigger': evo.chain.evolves_to[firstIndex].evolves_to[j].evolution_details.length > 0 ? evo.chain.evolves_to[firstIndex].evolves_to[j].evolution_details[0].trigger.name : null,
+        'level': evo.chain.evolves_to[firstIndex].evolves_to[j].evolution_details.length > 0 ? evo.chain.evolves_to[firstIndex].evolves_to[j].evolution_details[0].trigger.name !== 'level-up' ? null : evo.chain.evolves_to[firstIndex].evolves_to[j].evolution_details[0].min_level : null,
+      });
+    }
+    return secondEvolution;
+  }
+}
+
+function pushPokemon(info, species, chain) {
+  pokemon.push({
+    'id': info.id,
+    'origin': info.species.name,
+    'name': species.names[species.names.findLastIndex((element) => element.language.name == 'de')].name,
+    'type1': info.types[0].type.name,
+    'type2': info.types[1]?.type?.name ?? '',
+    'height': info.height,
+    'weight': info.weight,
+    'imgSmall': info.sprites.other.showdown.front_default == null ? info.sprites.front_default : info.sprites.other.showdown.front_default,
+    'imgBig': info.sprites.other.home.front_default,
+    'stats': {
+      'hp': info.stats[0].base_stat,
+      'attack': info.stats[1].base_stat,
+      'defense': info.stats[2].base_stat,
+      'spAttack': info.stats[3].base_stat,
+      'spDefense': info.stats[4].base_stat,
+      'speed': info.stats[5].base_stat,
+    },
+    'flavorText': species.flavor_text_entries[species.flavor_text_entries.findLastIndex((element) => element.language.name == 'de')]?.flavor_text ?? 'Über dieses Pokemon ist nichts bekannt',
+    'genus': species.genera[species.genera.findLastIndex((element) => element.language.name == 'de')]?.genus ?? 'Unbekannt',
+    'evolution': chain.id,
+  });
+}
+
+function pushType(typeInfo) {
+  types.push({
+    'id': typeInfo.id,
+    'origin': typeInfo.name,
+    'damage': typeInfo.damage_relations,
+    'name': typeInfo.names[typeInfo.names.findLastIndex((element) => element.language.name == 'de')]?.name ?? 'Unbekannt',
+    'img': `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-vii/lets-go-pikachu-lets-go-eevee/${typeInfo.id}.png`
+  });
+}
